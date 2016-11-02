0b34a87887b21d6a791700094eab6965152e0fe0
Jhadderson Hoyos <jhoyos@sitesas.co>
2016-11-02 14:47:11 -0500

    Cambios

--- task1.js
+++ task1.js
@@ -1,3 +1,16 @@
+    var meses = {"1": "Enero",
+                 "2": "Febrero",
+                 "3": "Marzo",
+                 "4": "Abril",
+                 "5": "Mayo",
+                 "6": "Junio",
+                 "7": "Julio",
+                 "8": "Agosto",
+                 "9": "Septiembre",
+                 "10": "Octubre",
+                 "11" : "Noviembre",
+                 "12" : "Diciembre"};
+
     var nuWidth,nuHeight
     var chartWidth, chartHeight
     var obMargin
